f6f9c75016493f3f2e07352123542dc527cb798b
hfzdzakii <muhammadhafizhdzaki@gmail.com>
2025-12-18 11:03:42 +0700

    <Trial&Error>

--- pages/Check Sheet Inspeksi ARM2.py
+++ pages/Check Sheet Inspeksi ARM2.py
@@ -37,7 +37,7 @@ st.header("NDT ARM")
 # ====
 for idx, (target, target_snake) in enumerate(list(zip(arm_target, arm_target_snake))[:2], start=1):
     st.subheader(f"{idx}\\. {target}", help="\n".join(str(i) for i in arm_data[target_snake].values()))
-    create_inspection_inputs2(target, target_snake)
+    create_inspection_inputs2(target)
 # ====
 st.subheader("Mechanic / Welder Comment")
 comment = st.text_area("📝 Masukkan Komentar Setelah Inspeksi!")
