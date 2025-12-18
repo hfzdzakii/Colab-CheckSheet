6f587f0fc649ea8a7f1f0f2e9b21d94d3c6cb6b5
hfzdzakii <muhammadhafizhdzaki@gmail.com>
2025-12-18 10:03:13 +0700

    <Trial&Error>

--- pages/Check Sheet Inspeksi ARM2.py
+++ pages/Check Sheet Inspeksi ARM2.py
@@ -20,50 +20,50 @@ with st.sidebar:
 
 st.title("Inspeksi ARM")
 
-with st.form("form_inspeksi_arm"):
-    st.header("Identitas")
-    id1, id2 = st.columns(2)
-    with id1:
-        nama = input_text("Nama")
-        code_unit = input_text("Code Unit")
-        egi = input_text("EGI")
-        district = input_text("District")
-    with id2:
-        hours_meter = input_text("Hours Meter")
-        date = st.date_input("Tanggal Insp", value="today", disabled=True, format="DD/MM/YYYY", help="Otomatis terisi hari ini!")
-        periode_service = input_radio("Periode Service", "periode")
-        
-    st.space("small")
-        
-    st.header("NDT ARM")
-    # ====
-    for idx, (target, target_snake) in enumerate(list(zip(arm_target, arm_target_snake))[:2], start=1):
-        st.subheader(f"{idx}\\. {target}", help="\n".join(str(i) for i in arm_data[target_snake].values()))
-        create_inspection_inputs2(target, target_snake)
-    # ====
-    st.subheader("Mechanic / Welder Comment")
-    comment = st.text_area("📝 Masukkan Komentar Setelah Inspeksi!")
+st.header("Identitas")
+id1, id2 = st.columns(2)
+with id1:
+    nama = input_text("Nama")
+    code_unit = input_text("Code Unit")
+    egi = input_text("EGI")
+    district = input_text("District")
+with id2:
+    hours_meter = input_text("Hours Meter")
+    date = st.date_input("Tanggal Insp", value="today", disabled=True, format="DD/MM/YYYY", help="Otomatis terisi hari ini!")
+    periode_service = input_radio("Periode Service", "periode")
     
-    submitted = st.form_submit_button("Save")
+st.space("small")
+
+st.header("NDT ARM")
+# ====
+for idx, (target, target_snake) in enumerate(list(zip(arm_target, arm_target_snake))[:2], start=1):
+    st.subheader(f"{idx}\\. {target}", help="\n".join(str(i) for i in arm_data[target_snake].values()))
+    create_inspection_inputs2(target, target_snake)
+# ====
+st.subheader("Mechanic / Welder Comment")
+comment = st.text_area("📝 Masukkan Komentar Setelah Inspeksi!")
     
+submitted = st.button("Save")
+    
+#  ===============================    
 identities = [nama, code_unit, egi, district, hours_meter, 
                 date, periode_service, comment]
 
-if submitted:
-    if any((field == 0.0 or field == None or field == "") for field in [*identities, *required_fields]):
-        st.error("❌ Ada input yang kosong. Silahkan diisi semuanya!")
-        st.stop()
-    for target in arm_target:
-        if st.session_state.images.get(target) is None:
-            st.error("❌ Ada gambar yang belum diambil. Silahkan ambil dokumentasi!")
-            st.stop()
-    st.session_state.form_submitted = True
-    st.session_state.pdf_download = False
+# if submitted:
+#     if any((field == 0.0 or field == None or field == "") for field in [*identities, *required_fields]):
+#         st.error("❌ Ada input yang kosong. Silahkan diisi semuanya!")
+#         st.stop()
+#     for target in arm_target:
+#         if st.session_state.images.get(target) is None:
+#             st.error("❌ Ada gambar yang belum diambil. Silahkan ambil dokumentasi!")
+#             st.stop()
+#     st.session_state.form_submitted = True
+#     st.session_state.pdf_download = False
 
-if st.session_state.form_submitted:
-    identities_processed = process_identities(identities, "inspection")
+# if st.session_state.form_submitted:
+#     identities_processed = process_identities(identities, "inspection")
     
-    required_fields_modified = [required_fields[i:i+4] for i in range(0, len(required_fields), 4)]
-    final_dict = dict(zip(arm_target, required_fields_modified))
+#     required_fields_modified = [required_fields[i:i+4] for i in range(0, len(required_fields), 4)]
+#     final_dict = dict(zip(arm_target, required_fields_modified))
     
     
