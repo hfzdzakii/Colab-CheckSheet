41e557f8c5fb516a9d943ad9b1136831dc230613
hfzdzakii <muhammadhafizhdzaki@gmail.com>
2025-12-26 20:22:01 +0700

    <Image>: Add new image into necessary part

--- pages/Check Sheet Inspeksi Boom.py
+++ pages/Check Sheet Inspeksi Boom.py
@@ -6,17 +6,18 @@ from functions.helper import page_config, init_state_inspection
 page_config()
 
 BASE_DIR = Path(__file__).resolve().parents[1]
-IMAGE_DIR = None
+IMAGE_DIR = BASE_DIR / "images"
 DATA_FILE = BASE_DIR / "data" / "data_inspeksi.json"
 PART_NAME = "Boom" # <== change this
+image_files = [f"NDT_{PART_NAME}_{i}.png" for i in range(1, 3)]
 
 data = load_data(DATA_FILE)[PART_NAME]
 target, target_snake = load_boom_inspection_target() # <== change this
 state_inspection = init_state_inspection(target)
 
 with st.sidebar:
-    st.subheader("Gambar1")
-    st.subheader("Gambar2")
-    st.subheader("Gambar3")
+    st.subheader(f"{PART_NAME}")
+    for i in image_files:
+        st.image(IMAGE_DIR / i)
     
 inspection_template(PART_NAME, data, target, target_snake)
